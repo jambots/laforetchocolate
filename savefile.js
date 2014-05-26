32d78760fd9e9814c25e7bb5504a97065962914f
jambots <jambotsdev@gmail.com>
2014-05-26 12:53:21 -0500

    error1

--- savefile.js
+++ savefile.js
@@ -50,6 +50,7 @@ function onDirectoryFail(error) {
  }
 }
 function filetransfer(download_link, fp) {
+dbuga("fp "+fp);
 var fileTransfer = new FileTransfer();
 // File download function with URL and local path
 fileTransfer.download(download_link, fp,
