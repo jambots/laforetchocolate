07166f8c68ed68c18b4df6000006f6796562df66
Terrance Schubring <tschubring@gmail.com>
2016-03-01 10:06:19 -0600

    debug 1 re jan 2016 test entry

--- includedfeed.js
+++ includedfeed.js
@@ -6,7 +6,7 @@ var includedFeed=[{
 		"status": "publish",
 		"title": "TestJan 2016",
 		"title_plain": "TestJan 2016",
-		"content": "<p><a href=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2.jpg\" rel=\"attachment wp-att-47\"><img src=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-232x300.jpg\" alt=\"Big Giant Graphic\" width=\"232\" height=\"300\" class=\"alignnone size-medium wp-image-47\" srcset=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-232x300.jpg 232w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-768x994.jpg 768w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-791x1024.jpg 791w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2.jpg 1275w\" sizes=\"(max-width: 232px) 100vw, 232px\" \/><\/a><a href=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical.jpg\" rel=\"attachment wp-att-48\"><img src=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical-225x300.jpg\" alt=\"Electrical Apparatus\" width=\"225\" height=\"300\" class=\"alignnone size-medium wp-image-48\" srcset=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical-225x300.jpg 225w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical.jpg 480w\" sizes=\"(max-width: 225px) 100vw, 225px\" \/><\/a><a href=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png\" rel=\"attachment wp-att-43\"><img src=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default-200x300.png\" alt=\"Portrait Tree\" width=\"200\" height=\"300\" class=\"alignnone size-medium wp-image-43\" srcset=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default-200x300.png 200w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png 320w\" sizes=\"(max-width: 200px) 100vw, 200px\" \/><\/a><a href=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png\" rel=\"attachment wp-att-45\"><img src=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png\" alt=\"Core Passion Icon\" width=\"88\" height=\"88\" class=\"alignnone size-full wp-image-45\" \/><\/a><a href=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1.png\" rel=\"attachment wp-att-41\"><img src=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1-300x300.png\" alt=\"Knight 1\" width=\"300\" height=\"300\" class=\"alignnone size-medium wp-image-41\" srcset=\"http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1-150x150.png 150w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1-300x300.png 300w, http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1.png 400w\" sizes=\"(max-width: 300px) 100vw, 300px\" \/><\/a><\/p>\n",
+		"content": "",
 		"excerpt": "",
 		"date": "2016-01-01 15:29:46",
 		"modified": "2016-03-01 15:51:50",
@@ -31,47 +31,6 @@ var includedFeed=[{
 		},
 		"comments": [],
 		"attachments": [{
-			"id": 45,
-			"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-			"slug": "cpl_88",
-			"title": "Core Passion Icon",
-			"description": "The app icon for Career Seeker.",
-			"caption": "",
-			"parent": 38,
-			"mime_type": "image\/png",
-			"images": {
-				"full": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-					"width": 88,
-					"height": 88
-				},
-				"thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-					"width": 88,
-					"height": 88
-				},
-				"medium": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-					"width": 88,
-					"height": 88
-				},
-				"medium_large": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-					"width": 88,
-					"height": 88
-				},
-				"large": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-					"width": 88,
-					"height": 88
-				},
-				"post-thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/cpl_88.png",
-					"width": 88,
-					"height": 88
-				}
-			}
-		}, {
 			"id": 41,
 			"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1.png",
 			"slug": "kni_n_1",
@@ -110,119 +69,6 @@ var includedFeed=[{
 					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/kni_n_1.png",
 					"width": 400,
 					"height": 400
-				}
-			}
-		}, {
-			"id": 43,
-			"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png",
-			"slug": "default",
-			"title": "Portrait Tree",
-			"description": "A default image for the app.",
-			"caption": "",
-			"parent": 38,
-			"mime_type": "image\/png",
-			"images": {
-				"full": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png",
-					"width": 320,
-					"height": 480
-				},
-				"thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default-150x150.png",
-					"width": 150,
-					"height": 150
-				},
-				"medium": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default-200x300.png",
-					"width": 200,
-					"height": 300
-				},
-				"medium_large": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png",
-					"width": 320,
-					"height": 480
-				},
-				"large": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png",
-					"width": 320,
-					"height": 480
-				},
-				"post-thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/Default.png",
-					"width": 320,
-					"height": 480
-				}
-			}
-		}, {
-			"id": 47,
-			"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2.jpg",
-			"slug": "mycorepassion-nav-v2-0-2",
-			"title": "Big Giant Graphic",
-			"description": "Gigantic image jpeg portrait.",
-			"caption": "",
-			"parent": 38,
-			"mime_type": "image\/jpeg",
-			"images": {
-				"full": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2.jpg",
-					"width": 1275,
-					"height": 1650
-				},
-				"thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-150x150.jpg",
-					"width": 150,
-					"height": 150
-				},
-				"medium": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-232x300.jpg",
-					"width": 232,
-					"height": 300
-				},
-				"post-thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/MYCOREPASSION-NAV-V2-0-2-825x510.jpg",
-					"width": 825,
-					"height": 510
-				}
-			}
-		}, {
-			"id": 48,
-			"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical.jpg",
-			"slug": "electrical",
-			"title": "Electrical Apparatus",
-			"description": "",
-			"caption": "",
-			"parent": 38,
-			"mime_type": "image\/jpeg",
-			"images": {
-				"full": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical.jpg",
-					"width": 480,
-					"height": 640
-				},
-				"thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical-150x150.jpg",
-					"width": 150,
-					"height": 150
-				},
-				"medium": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical-225x300.jpg",
-					"width": 225,
-					"height": 300
-				},
-				"medium_large": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical.jpg",
-					"width": 480,
-					"height": 640
-				},
-				"large": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical.jpg",
-					"width": 480,
-					"height": 640
-				},
-				"post-thumbnail": {
-					"url": "http:\/\/www.chromaris.org\/wp-content\/uploads\/2016\/01\/electrical-480x510.jpg",
-					"width": 480,
-					"height": 510
 				}
 			}
 		}],
